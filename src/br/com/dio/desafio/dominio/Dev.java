f33cfaf49b7e30735c70011bf5e278bff48aabb2
DiegoBeker <diegobecker2007@hotmail.com>
2022-11-08 21:08:25 -0300

    final

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -7,20 +7,20 @@ import java.util.Set;
 
 public class Dev {
     private String nome;
-    private Set<Conteudo> conteudosInscrtios = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-        this.conteudosInscrtios.addAll(bootcamp.getConteudos());
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-        Optional<Conteudo> conteudo = this.conteudosInscrtios.stream().findFirst();
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
 
         if(conteudo.isPresent()) {
             this.conteudosConcluidos.add(conteudo.get());
-            this.conteudosInscrtios.remove(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
         }else{
             System.err.println("Você não está matriculado em nunhum conteúdo!");
         }
@@ -38,12 +38,12 @@ public class Dev {
         this.nome = nome;
     }
 
-    public Set<Conteudo> getConteudosInscrtios() {
-        return conteudosInscrtios;
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
     }
 
-    public void setConteudosInscrtios(Set<Conteudo> conteudosInscrtios) {
-        this.conteudosInscrtios = conteudosInscrtios;
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscrtios) {
+        this.conteudosInscritos = conteudosInscrtios;
     }
 
     public Set<Conteudo> getConteudosConcluidos() {
@@ -59,11 +59,11 @@ public class Dev {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscrtios, dev.conteudosInscrtios) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudosInscrtios, conteudosConcluidos);
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
     }
 }
